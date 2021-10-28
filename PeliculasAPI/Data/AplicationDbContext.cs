1adeb700dae3afb300dfa918082a7e0d4e690642
JASON JESUS GONZALEZ SALAS <jason.gonzalez1@uamcr.net>
2021-10-27 21:34:44 -0600

    Se agrega la carpeta de Data.

---
+++ PeliculasAPI/Data/AplicationDbContext.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.EntityFrameworkCore;
+using PeliculasAPI.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace PeliculasAPI.Data
+{
+    public class AplicationDbContext : DbContext
+    {
+        public AplicationDbContext(DbContextOptions<AplicationDbContext> option ) : base(option)
+        {   
+
+        }
+        public DbSet<Categoria> Categorias { get; set; }
+    }
+}
--- PeliculasAPI/Models/Categoria.cs
+++ PeliculasAPI/Models/Categoria.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Threading.Tasks;
 
@@ -7,5 +8,10 @@ namespace PeliculasAPI.Models
 {
     public class Categoria
     {
+        [Key]
+        public int Id { get; set; }
+        public string Nombre { get; set; }
+
+        public bool Estado { get; set; }
     }
 }
