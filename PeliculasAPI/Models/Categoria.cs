3d8385f3024e4b8d8f13e107c23d0c77905e9fca
JASON JESUS GONZALEZ SALAS <jason.gonzalez1@uamcr.net>
2021-10-27 22:02:11 -0600

    Migrations Addedd

--- PeliculasAPI/Models/Categoria.cs
+++ PeliculasAPI/Models/Categoria.cs
@@ -10,8 +10,9 @@ namespace PeliculasAPI.Models
     {
         [Key]
         public int Id { get; set; }
+        [Required]
         public string Nombre { get; set; }
-
+        [Required]
         public bool Estado { get; set; }
     }
 }
