c04ee8e99455e0f1c5e451d1f9590ea2971f523f
JASON JESUS GONZALEZ SALAS <jason.gonzalez1@uamcr.net>
2021-10-27 22:21:41 -0600

    Primer API

---
+++ PeliculasAPI/Controllers/CategoriaController.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using PeliculasAPI.Data;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace PeliculasAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class CategoriaController : ControllerBase
+    {
+        private readonly AplicationDbContext _db;
+        public CategoriaController(AplicationDbContext db)
+        {
+            _db = db;
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> GetCategorias()
+        {
+            var lista = await _db.Categorias.OrderBy(c => c.Nombre).ToListAsync();
+            return Ok(lista);
+        }
+    }
+}
